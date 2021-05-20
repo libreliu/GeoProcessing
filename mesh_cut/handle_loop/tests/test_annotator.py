0a6e2b594bb9173bbd305e5edc389bd91e14cb05
Libre Liu <libreliu@foxmail.com>
2021-05-21 05:27:04 +0800

    working visualization of h1(o) on genus1.obj

--- mesh_cut/handle_loop/tests/test_annotator.py
+++ mesh_cut/handle_loop/tests/test_annotator.py
@@ -1,5 +1,7 @@
+from mesh_cut.handle_loop.sp_tree import SpanningTree
 from mesh_cut.handle_loop.graphbase import *
 from mesh_cut.handle_loop.annotator import Annotator
+from mesh_cut.handle_loop.main import offscreen_combine_plot, combine_plot, om_to_vis_polydata, lines_to_vis_polydata
 import unittest
 import openmesh as om
 
@@ -25,3 +27,44 @@ class AnnotatorTest(unittest.TestCase):
         annotation, null_vector = annotator.compute_annotation()
 
         print(annotation)
+    
+    def test_vis_homology_basis(self):
+        voluGraphBase = GraphBase.volumetric_from_openmesh(self.meshes['genus1'])
+        annotator = Annotator(voluGraphBase)
+
+        sp_tree = SpanningTree(voluGraphBase)
+        sp_tree.build_mst()
+        
+        bbasis, bpivot = annotator.get_boundary_basis()
+        cbasis = annotator.get_cycle_basis(sp_tree)
+        dim_boundary = len(bpivot)
+        
+        z_tilde, dim_h1 = annotator.get_h1_basis(bbasis, cbasis, bpivot)
+        h1_vec = z_tilde[:, (z_tilde.shape[1] - dim_h1):].reshape(voluGraphBase.n_edges)
+
+        print(h1_vec)
+
+        base_data = om_to_vis_polydata(self.meshes['genus1'])
+        edge_data = lines_to_vis_polydata(
+            voluGraphBase._points,
+            voluGraphBase.edge_list_from_vector(h1_vec)
+        )
+        combine_plot(
+            (
+                edge_data,
+                {
+                'color': 'red',
+                'line_width': 3.0
+                }
+            ),
+            (
+                base_data,
+                {
+                'color': 'tan',
+                'opacity': 0.5,
+                'style': 'surface',
+                'show_edges': True
+                }
+            )
+        )
+
